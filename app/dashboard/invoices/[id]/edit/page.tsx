b00e6e0fad9fdc162fc03865fa0837e7ce20a0ba
Gee Chai <fc00004@gmail.com>
2023-12-08 22:07:54 +0900

    feat: basic error messages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,28 +1,32 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-
-import { customers } from '@/app/lib/placeholder-data';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { updateInvoice } from '@/app/lib/actions';
+import {notFound} from 'next/navigation';
 
-export default async function Page({params} : {params: {id: string}}) {
-    const id = params.id
+export default async function Page({params}: {params: { id: string }}) {
+    const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers()
     ])
-    return (
-        <main>
-            <Breadcrumbs
-            breadcrumbs={[
-                {label: 'Invoices', href: '/dashboard/invoices'},
-                {
-                    label: 'Edit Invoice', 
-                    href: `/dashboard/invoices/${id}/edit`,
-                    active: true
-                },
-            ]}
-            />
-            <Form invoice={invoice} customers={customers}/>
-        </main>
-    )
+
+    if(!invoice){
+      notFound();
+    }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
 }
